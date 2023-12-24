90c655a8c304b775afad5184ac3a6da8901a9633
Daniel-J101 <frostysausage123@gmail.com>
2023-12-23 21:43:20 -0600

    running changes

---
+++ src/main/java/frc/spectrumLib/mechanism/MechanismTelemetry.java
@@ -0,0 +1,22 @@
+package frc.spectrumLib.mechanism;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public final class MechanismTelemetry {
+    protected ShuffleboardTab tab = Shuffleboard.getTab("Mechanism Telemetry"); //TODO: fix
+    private boolean initialized = false;
+    
+    private MechanismTelemetry() {}
+
+    public void testMode() {
+        if(!initialized) {
+            tab.add("Automatic System Check", false).withWidget("Toggle Button").withSize(2, 1).withPosition(0, 0);
+            initialized = true;
+        }
+    }
+
+    public void runSystemCheck() {
+
+    }
+}

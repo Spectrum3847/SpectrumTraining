3a47f409382843747fa948122f413f5ec82bd9a5
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2024-01-10 16:41:38 -0600

    updated

--- src/main/java/frc/robot/intake/Intake.java
+++ src/main/java/frc/robot/intake/Intake.java
@@ -1,22 +1,37 @@
 package frc.robot.intake;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.spectrumLib.mechanism.Mechanism;
 import frc.spectrumLib.mechanism.TalonFXFactory;
 
 public class Intake extends Mechanism {
     public class IntakeConfig extends Config {
+        // Intake speeds in rpm
+        public double falconMaxSpeed = 6380; // RPM
+        public double intake = 6500; // 2000
+        public double slowIntake = 900; // 600
+        public double slowIntakePercentage =
+                0.06; // This is used instead of a velocity to lower hold current
+        public double holdIntakePercentage =
+                0.1; // This is used instead of a velocity to lower hold current
+        public double eject = -3000;
+        public double drop = -300;
+        public double floorDrop = -500;
 
-        public double fullSpeed = 1.0;
-        public double ejectSpeed = -1.0;
+        public final double velocityKp = 0.5;
+        public final double velocityKv = 0.5;
+        public final double currentLimit = 20;
 
         public IntakeConfig() {
-            super("Intake", 60, "3847");
-            configPIDGains(0, 0.55, 0, 0.2);
-            configFeedForwardGains(0, 0, 0, 0);
-            configGearRatio(2);
-            configSupplyCurrentLimit(20, true);
+            super("Intake", 52, "rio");
+            configPIDGains(0, velocityKp, 0, 0);
+            configFeedForwardGains(0, velocityKv, 0, 0);
+            configMotionMagic(120, 195, 50); // TODO: configure jerk
+            configSupplyCurrentLimit(currentLimit, true);
             configClockwise_Positive();
+            // TODO: config nominal output?
         }
     }
 
@@ -27,18 +42,16 @@ public class Intake extends Mechanism {
         if (attached) {
             motor = TalonFXFactory.createConfigTalon(config.id, config.talonConfig);
         }
-    }
+    }    
 
     @Override
     public void periodic() {}
 
-    public Command runVelocity(double velocity) {
-        return run(() -> setMMVelocityFOC(velocity)).withName("Intake.runVelocity");
+    public Command runPercent(double percent) {
+        return run(() -> setPercentOutput(percent)).withName("Intake.runPercent");
     }
 
-    @Override
-    protected Config setConfig() {
-        config = new IntakeConfig();
-        return config;
-    }
+    
+
+
 }

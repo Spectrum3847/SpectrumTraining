e510554d131177cd07607dcbb654a283ef24ead4
Daniel-J101 <frostysausage123@gmail.com>
2024-01-20 16:07:02 -0600

    added intake
    squashed merge from robot/FM-intake

--- src/main/java/frc/robot/intake/IntakeCommands.java
+++ src/main/java/frc/robot/intake/IntakeCommands.java
@@ -7,14 +7,40 @@ public class IntakeCommands {
     private static Intake intake = Robot.intake;
 
     public static void setupDefaultCommand() {
-        intake.setDefaultCommand(intake.runVelocity(0).withName("Intake.default"));
+        intake.setDefaultCommand(holdPercentOutput().withName("Intake.default"));
     }
 
     public static Command runFull() {
-        return intake.runVelocity(intake.config.fullSpeed);
+        return intake.runVelocity(intake.config.maxSpeed).withName("Intake.runFull");
+    }
+
+    public static Command slowIntake() {
+        return intake.runPercentage(intake.config.slowIntakePercentage)
+                .withName("Intake.slowIntake");
+    }
+
+    public static Command holdPercentOutput() {
+        return intake.runPercentage(intake.config.holdIntakePercentage)
+                .withName("Intake.holdPercentOutput");
+    }
+
+    public static Command intake() {
+        return intake.runVelocity(intake.config.intake).withName("Intake.intake");
     }
 
     public static Command eject() {
-        return intake.runVelocity(intake.config.ejectSpeed);
+        return intake.runVelocity(intake.config.eject).withName("Intake.eject");
+    }
+
+    public static Command drop() {
+        return intake.runVelocity(intake.config.drop).withName("Intake.drop");
+    }
+
+    public static Command floorDrop() {
+        return intake.runVelocity(intake.config.floorDrop).withName("Intake.floorDrop");
+    }
+
+    public static Command stopMotor() {
+        return intake.runStop().withName("Intake.stopMotor");
     }
 }
